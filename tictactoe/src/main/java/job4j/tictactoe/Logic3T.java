ea11c16e04827e14206f669551674bcf0924b203
Vadim Semenov <5587394@mail.ru>
2020-04-13 12:36:30 +0300

    Крестики-нолики на JavaFX[#131682]

--- tictactoe/src/main/java/job4j/tictactoe/Logic3T.java
+++ tictactoe/src/main/java/job4j/tictactoe/Logic3T.java
@@ -25,31 +25,26 @@ public class Logic3T {
     }
 
     public boolean isWinnerX() {
-        return this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 0) ||
-                this.fillBy(Figure3T::hasMarkX, 0, 0, 0, 1) ||
-
-                this.fillBy(Figure3T::hasMarkX, 0, 1, 1, 0) ||
-                this.fillBy(Figure3T::hasMarkX, 1, 0, 0, 1) ||
-
-                this.fillBy(Figure3T::hasMarkX, 0, 2, 1, 0) ||
-                this.fillBy(Figure3T::hasMarkX, 2, 0, 0, 1) ||
-
-                this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 1) ||
-                this.fillBy(Figure3T::hasMarkX, this.table.length - 1, 0, -1, 1);
+        return this.isWinner(Figure3T::hasMarkX);
     }
 
     public boolean isWinnerO() {
-        return this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 0) ||
-                this.fillBy(Figure3T::hasMarkO, 0, 0, 0, 1) ||
+        return this.isWinner(Figure3T::hasMarkO);
+    }
 
-                this.fillBy(Figure3T::hasMarkO, 0, 1, 1, 0) ||
-                this.fillBy(Figure3T::hasMarkO, 1, 0, 0, 1) ||
+    public boolean isWinner(Predicate<Figure3T> predicate) {
 
-                this.fillBy(Figure3T::hasMarkO, 0, 2, 1, 0) ||
-                this.fillBy(Figure3T::hasMarkO, 2, 0, 0, 1) ||
+        return this.fillBy(predicate, 0, 0, 1, 0) ||
+                this.fillBy(predicate, 0, 0, 0, 1) ||
 
-                this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 1) ||
-                this.fillBy(Figure3T::hasMarkO, this.table.length - 1, 0, -1, 1);
+                this.fillBy(predicate, 0, 1, 1, 0) ||
+                this.fillBy(predicate, 1, 0, 0, 1) ||
+
+                this.fillBy(predicate, 0, 2, 1, 0) ||
+                this.fillBy(predicate, 2, 0, 0, 1) ||
+
+                this.fillBy(predicate, 0, 0, 1, 1) ||
+                this.fillBy(predicate, this.table.length - 1, 0, -1, 1);
     }
 
     public boolean hasGap() {
